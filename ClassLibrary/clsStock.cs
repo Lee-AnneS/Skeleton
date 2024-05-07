e81054a573edf2bf91aa374bebd75c7b96efa01e
Alexandre De Menezes <P2724348@my365.dmu.ac.uk>
2024-05-07 14:02:47 +0100

    Creating the Properties for tblProducts table

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Available { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public int StockQuantity { get; set; }
+        public int ProductId { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
