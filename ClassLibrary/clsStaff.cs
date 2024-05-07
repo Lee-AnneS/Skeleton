1cedf8975e2874eb5b0093136fa6f769e3124c9b
p17206496 <p17206496@MY365.dmu.ac.uk>
2024-05-07 17:09:55 +0100

    I have created the tests from practical 7

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,24 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+
+        public int StaffID { get; set; }
+
+        public String StaffFullName { get; set; }
+
+        public DateTime StaffDoB { get; set; }
+
+        public string StaffEmail { get; set; }
+
+        public string NINumber { get; set; }
+
+        public double Salary { get; set; }
+
+        public bool PresentInBuilding { get; set; }
+
     }
 }
