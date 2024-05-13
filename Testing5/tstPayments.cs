bcaa4cd21b9bc470fda5a60939a61c85cb3b2e7a
itsnot6abriela <P2726046@my365.dmu.ac.uk>
2024-05-13 04:12:36 +0100

    pratical 9a

--- Testing5/tstPayments.cs
+++ Testing5/tstPayments.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using System;
 using System.Security.Policy;
+using static System.Net.WebRequestMethods;
 
 
 
@@ -105,12 +106,181 @@ namespace Testing5
             //create an instance of the class we want to createe 
             clsPayments APayments = new clsPayments();
             //create some test data to assign to the property 
-            double TestData = 3.45;
+            double TestData = 2.22;
             //assign the data to the property 
             APayments.Amount = TestData;
             //test to see that the two values are the same 
             Assert.AreEqual(APayments.Amount, TestData);
 
+        }
+        // ......................find test method ...................
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //Create an instance of the class we want to create
+            clsPayments APayments = new clsPayments();
+            //create a boolean variable to store the results of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            Int32 PaymentsId = 22;
+            //invoke the method
+            Found = APayments.Find(PaymentsId);
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
+        // ...................property data tests.................
+        [TestMethod]
+        public void TestPaymentsIdFound()
+        {  //create an instance of the class we want to create
+            clsPayments APayments = new clsPayments();
+            //create a boolean variable to store the result of the serach
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 PaymentsId = 22;
+            //invoke the method
+            Found = APayments.Find(PaymentsId);
+            //check the payments ID
+            if (APayments.PaymentsId != 22) 
+            { 
+                OK = false; 
+            }
+            // test to see that the result is correct
+            Assert.IsTrue(OK);  
+        }
+        [TestMethod]
+        public void TestAmountFound()
+        {  //create an instance of the class we want to create
+            clsPayments APayments = new clsPayments();
+            //create a boolean variable to store the result of the serach
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 PaymentsId = 22;
+            //invoke the method
+            Found = APayments.Find(PaymentsId);
+            //check the amounts
+            if (APayments.Amount != 2.22)
+            {
+                OK = false;
+            }
+            // test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestPaymentsDateFound()
+        {
+            // create an instance of the class we want to create
+            clsPayments APayments = new clsPayments();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            // create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            // create some test data to use with the method
+            Int32 PaymentsId = 22;
+            //invoke method
+            Found = APayments.Find(PaymentsId);
+            // check the dateadded property
+            if (APayments.PaymentsDate != Convert.ToDateTime("22/02/2022"))
+            {
+                OK = false;
+            }
+            // Test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestOrderIdFound()
+        {  //create an instance of the class we want to create
+            clsPayments APayments = new clsPayments();
+            //create a boolean variable to store the result of the serach
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            // create some test data to use with the method
+            Int32 PaymentsId = 22;
+            //create some test data to use with the method
+            Int32 OrderId = 20;
+            //invoke the method
+            Found = APayments.Find(OrderId);
+            //check the order ID
+            if (APayments.OrderId != 20)
+            {
+                OK = false;
+            }
+            // test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestBillingAddressFound()
+        {  //create an instance of the class we want to create
+            clsPayments APayments = new clsPayments();
+            //create a boolean variable to store the result of the serach
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            //create some test data to use with this method
+            Int32 PaymentsId = 22;
+
+            //create some test data to use with the method
+            //string BillingAddress = "Flat 6 St.Matthews house NG4 5LX";
+            //invoke the method
+            Found = APayments.Find(PaymentsId);
+            //check the billing address 
+            if (APayments.BillingAddress != "Flat 6 St.Matthews house NG4 5LX")
+            {
+                OK = false;
+            }
+            // test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestPaymentsStatusFound()
+        {  //create an instance of the class we want to create
+            clsPayments APayments = new clsPayments();
+            //create a boolean variable to store the result of the serach
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+
+            //create some test data to use with this method
+            Int32 PaymentsId = 22;
+
+            //invoke the method
+            Found = APayments.Find(PaymentsId);
+            //check the billing address ID
+            if (APayments.PaymentsStatus != true)
+            {
+                OK = false;
+            }
+            // test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestPaymentMethodFound()
+        {  //create an instance of the class we want to create
+            clsPayments APayments = new clsPayments();
+            //create a boolean variable to store the result of the serach
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 PaymentsId = 22;
+            //invoke the method
+            Found = APayments.Find(PaymentsId);
+            //check the payment method
+            if (APayments.PaymentMethod != "Apple Pay")
+            {
+                OK = false;
+            }
+            // test to see that the result is correct
+            Assert.IsTrue(OK);
         }
 
     }
